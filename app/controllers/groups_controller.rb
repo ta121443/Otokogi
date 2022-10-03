459a50d89c46dd5ccfded10e33d22f3202dd9821
内山遼大 <uchiyamaryota@TEC-022-0067.local>
2022-10-03 22:54:45 +0900

    グループ追加画面の作成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,22 @@
 class GroupsController < ApplicationController
+  def new 
+    @group = Group.new
+  end
+
+  def create 
+    @group = Group.new(group_params)
+    if @group.save
+      flash[:success] = "グループを追加しました"
+      redirect_to top_path
+    else
+      render 'new'
+    end
+    
+  end
+
+  private 
+
+  def group_params 
+    params.require(:group).permit(:name, :session_id)
+  end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  validates :name, presence: true
+  validates :session_id, presence: true
 end
