69423ea6aa19a3102ca0f3e349ee00a8f71359b8
内山遼大 <uchiyamaryota@TEC-022-0067.local>
2022-10-03 23:23:14 +0900

    ユーザー作成画面の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,24 @@ class UsersController < ApplicationController
   end
   
   def new
-    
+    @user = User.new
+    @groups = Group.all
   end
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ユーザーを追加しました"
+      redirect_to top_path
+    else
+      render 'new'
+    end
+  end
+
+  private 
+
+    def user_params 
+      params.require(:user).permit(:name, :group_id, :session_id)
+    end
+
 end
